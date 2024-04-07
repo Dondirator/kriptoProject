876f5e65843b8e475717bb5dc0b5a591229bc47c
Dondirator <75539044+Dondirator@users.noreply.github.com>
2024-04-07 23:02:00 +0300

    421

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -1,26 +1,15 @@
 import { provideTransloco, TranslocoModule } from '@ngneat/transloco';
-import { isDevMode, InjectionToken, NgModule } from '@angular/core';
+import { Injectable, isDevMode, NgModule } from '@angular/core';
 import { TranslocoHttpLoader } from './transloco-loader';
-
-// Создайте InjectionToken для TRANSLOCO_TRANSPILER
-export const TRANSLOCO_TRANSPILER = new InjectionToken<string>(
-  'TRANSLOCO_TRANSPILER'
-);
 
 @NgModule({
   exports: [TranslocoModule],
   providers: [
-    // Предоставьте значение для TRANSLOCO_TRANSPILER
-    { provide: TRANSLOCO_TRANSPILER, useValue: 'your-value' }, // Замените 'your-value' на соответствующее значение
     provideTransloco({
       config: {
         availableLangs: ['ru', 'en'],
         defaultLang: 'ru',
-        fallbackLang: ['ru'],
-        missingHandler: {
-          useFallbackTranslation: true,
-        },
-        // Remove this option if your application doesn't support changing language in runtime.
+
         reRenderOnLangChange: true,
         prodMode: !isDevMode(),
       },
