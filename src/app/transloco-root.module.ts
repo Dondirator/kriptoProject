d4349ff51a63a737126e7acb9f014b04355eaf41
Dondirator <75539044+Dondirator@users.noreply.github.com>
2024-04-07 22:43:01 +0300

    421412

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -1,10 +1,17 @@
 import { provideTransloco, TranslocoModule } from '@ngneat/transloco';
-import { isDevMode, NgModule } from '@angular/core';
+import { isDevMode, InjectionToken, NgModule } from '@angular/core';
 import { TranslocoHttpLoader } from './transloco-loader';
+
+// Создайте InjectionToken для TRANSLOCO_TRANSPILER
+export const TRANSLOCO_TRANSPILER = new InjectionToken<string>(
+  'TRANSLOCO_TRANSPILER'
+);
 
 @NgModule({
   exports: [TranslocoModule],
   providers: [
+    // Предоставьте значение для TRANSLOCO_TRANSPILER
+    { provide: TRANSLOCO_TRANSPILER, useValue: 'your-value' }, // Замените 'your-value' на соответствующее значение
     provideTransloco({
       config: {
         availableLangs: ['ru', 'en'],
